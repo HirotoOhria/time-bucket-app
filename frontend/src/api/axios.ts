251ca77ac0b43729dc244cca529e08937bd8a991
ohira.hiroto <hiroto100114@gmail.com>
2022-08-31 23:52:26 +0900

    Add BackendErrorResponse.

--- frontend/src/api/axios.ts
+++ frontend/src/api/axios.ts
@@ -1,8 +1,8 @@
 import axios from 'axios';
 
-type AxiosResponse<T> = {
-  data: T;
-}
+export type BackendErrorResponse = {
+  message: string;
+};
 
 export const baseAxios = axios.create({
   baseURL: 'http://localhost:8080', // バックエンドB のURL:port を指定する
--- frontend/src/api/bucket_item_api.ts
+++ frontend/src/api/bucket_item_api.ts
@@ -1,21 +1,21 @@
-import axios, { AxiosResponse } from 'axios';
+import axios, { AxiosError, AxiosResponse } from 'axios';
 import useSWR from 'swr';
 import { BucketItem } from '../domain/entity/bucket_item';
-import { baseAxios } from './axios';
+import { BackendErrorResponse, baseAxios } from './axios';
 
 type BucketItemResponse = {
   bucketItem?: BucketItem;
   isLoading: boolean;
-  isError?: Error;
+  isError?: AxiosError<BackendErrorResponse>;
 };
 
 const fetcher = (url: string) => baseAxios.get(url).then((res) => res.data);
 
 export const useBucketItem = (): BucketItemResponse => {
-  const { data, error } = useSWR<AxiosResponse<BucketItem>, Error>(
-    '/bucket_item/01GBMPC11X0G8BK9XB5BYVJ5EM',
-    fetcher
-  );
+  const { data, error } = useSWR<
+    AxiosResponse<BucketItem>,
+    AxiosError<BackendErrorResponse>
+  >('/bucket_item/01GBMPC11X0G8BK9XB5BYVJ5EM', fetcher);
 
   return {
     bucketItem: data?.data,
