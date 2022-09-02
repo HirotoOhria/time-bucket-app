3cc606933348cd191fddce7764b08ec455a4c340
ohira.hiroto <hiroto100114@gmail.com>
2022-09-02 12:49:53 +0900

    Add wire to install.sh.

--- tools/install.sh
+++ tools/install.sh
@@ -2,6 +2,7 @@
 
 echo "start tool install" &&
 
+go install github.com/google/wire/cmd/wire@latest
 go install github.com/schemalex/schemalex/cmd/schemadiff@latest &&
 go install github.com/schemalex/schemalex/cmd/schemalex@latest &&
 go install github.com/schemalex/schemalex/cmd/schemalint@latest &&
